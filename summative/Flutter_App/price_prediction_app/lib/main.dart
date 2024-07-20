34c0282e90a7feea0280acd1e31bcca907f45f08
k-ganda <k.ganda@alustudent.com>
2024-07-20 10:25:17 +0300

    flutter_app_integration

--- summative/Flutter_App/price_prediction_app/lib/main.dart
+++ summative/Flutter_App/price_prediction_app/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
 
@@ -7,37 +10,85 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            centerTitle: true,
-            backgroundColor: Colors.pink,
-            title: const Text(
-              "Insurance cost Prediction App",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-              ),
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.pink,
+          title: const Text(
+            "Insurance cost Prediction App",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
             ),
           ),
-          body: SingleChildScrollView(
-            child: PredictionForm(),
-          )),
+        ),
+        body: const SingleChildScrollView(
+          child: PredictionForm(),
+        ),
+      ),
     );
   }
 }
 
 class PredictionForm extends StatefulWidget {
+  const PredictionForm({super.key});
   @override
   _PredictionFormState createState() => _PredictionFormState();
 }
 
 class _PredictionFormState extends State<PredictionForm> {
+  // Text editing controllers
   final TextEditingController ageController = TextEditingController();
   final TextEditingController sexController = TextEditingController();
   final TextEditingController bmiController = TextEditingController();
   final TextEditingController childrenController = TextEditingController();
   final TextEditingController smokerController = TextEditingController();
   final TextEditingController regionController = TextEditingController();
+  String result = ''; // stores the Predicted insurance cost
+
+  Future<void> predict() async {
+    final String url =
+        'https://price-prediction-model-4ns5.onrender.com/predict';
+    final Map<String, dynamic> data = {
+      'age': int.parse(ageController.text),
+      'sex': int.parse(sexController.text),
+      'bmi': double.parse(bmiController.text),
+      'children': int.parse(childrenController.text),
+      'smoker': int.parse(smokerController.text),
+      'region': int.parse(regionController.text),
+    };
+
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode(data),
+      );
+
+      if (response.statusCode == 200) {
+        final responseData =
+            json.decode(response.body); // converts JSON to dart object
+        setState(() {
+          double charges = responseData['predicted_charges'];
+          final formatter = NumberFormat.currency(
+              locale: 'en_US', symbol: '\$', decimalDigits: 2);
+          result = 'Predicted Charges: ${formatter.format(charges)}';
+        });
+      } else {
+        final responseData = json.decode(response.body);
+        String errorMessage = responseData['detail'].map((error) {
+          return '${error['loc'][1]}: ${error['msg']} (got ${error['input']})';
+        }).join(', ');
+        setState(() {
+          result = 'Error: $errorMessage';
+        });
+      }
+    } catch (e) {
+      setState(() {
+        result = 'Error: $e';
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -73,26 +124,20 @@ class _PredictionFormState extends State<PredictionForm> {
           const SizedBox(height: 20),
           TextFormField(
             controller: ageController,
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               labelText: 'Age',
               border: OutlineInputBorder(
                 borderRadius: BorderRadius.all(Radius.circular(10.0)),
                 borderSide: BorderSide(color: Colors.pink),
               ),
-              hintText: 'Enter your age(18+)',
+              hintText: 'Enter your age (18+)',
             ),
             keyboardType: TextInputType.number,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter your age';
-              }
-              return null;
-            },
           ),
           const SizedBox(height: 20),
           TextFormField(
-            controller: ageController,
-            decoration: InputDecoration(
+            controller: sexController,
+            decoration: const InputDecoration(
               labelText: 'Sex',
               border: OutlineInputBorder(
                 borderRadius: BorderRadius.all(Radius.circular(10.0)),
@@ -101,17 +146,11 @@ class _PredictionFormState extends State<PredictionForm> {
               hintText: 'Enter (0 for male, 1 for female)',
             ),
             keyboardType: TextInputType.number,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter your sex';
-              }
-              return null;
-            },
           ),
           const SizedBox(height: 20),
           TextFormField(
-            controller: ageController,
-            decoration: InputDecoration(
+            controller: bmiController,
+            decoration: const InputDecoration(
               labelText: 'BMI',
               border: OutlineInputBorder(
                 borderRadius: BorderRadius.all(Radius.circular(10.0)),
@@ -120,55 +159,37 @@ class _PredictionFormState extends State<PredictionForm> {
               hintText: 'Enter your Body Mass Index',
             ),
             keyboardType: TextInputType.number,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter your BMI';
-              }
-              return null;
-            },
           ),
           const SizedBox(height: 20),
           TextFormField(
-            controller: ageController,
-            decoration: InputDecoration(
+            controller: childrenController,
+            decoration: const InputDecoration(
               labelText: 'Children',
               border: OutlineInputBorder(
                 borderRadius: BorderRadius.all(Radius.circular(10.0)),
                 borderSide: BorderSide(color: Colors.pink),
               ),
-              hintText: 'Enter the no of children ',
+              hintText: 'Enter the number of children',
             ),
             keyboardType: TextInputType.number,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter no of children';
-              }
-              return null;
-            },
           ),
           const SizedBox(height: 20),
           TextFormField(
-            controller: ageController,
-            decoration: InputDecoration(
+            controller: smokerController,
+            decoration: const InputDecoration(
               labelText: 'Smoker',
               border: OutlineInputBorder(
                 borderRadius: BorderRadius.all(Radius.circular(10.0)),
                 borderSide: BorderSide(color: Colors.pink),
               ),
-              hintText: 'Enter(0 for no, 1 for yes)',
+              hintText: 'Enter (0 for no, 1 for yes)',
             ),
             keyboardType: TextInputType.number,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter your smoking status';
-              }
-              return null;
-            },
           ),
           const SizedBox(height: 20),
           TextFormField(
-            controller: ageController,
-            decoration: InputDecoration(
+            controller: regionController,
+            decoration: const InputDecoration(
               labelText: 'Region',
               border: OutlineInputBorder(
                 borderRadius: BorderRadius.all(Radius.circular(10.0)),
@@ -177,20 +198,25 @@ class _PredictionFormState extends State<PredictionForm> {
               hintText: 'Enter (0-NW, 1-NE, 2-SE, 3-SW)',
             ),
             keyboardType: TextInputType.number,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter your region';
-              }
-              return null;
-            },
           ),
           const SizedBox(height: 20),
           Center(
             child: SizedBox(
-              width: 200, // Set your desired width here
+              width: 200,
               child: ElevatedButton(
-                onPressed: () {
-                  // Add your prediction logic here
+                onPressed: () async {
+                  if (ageController.text.isEmpty ||
+                      sexController.text.isEmpty ||
+                      bmiController.text.isEmpty ||
+                      childrenController.text.isEmpty ||
+                      smokerController.text.isEmpty ||
+                      regionController.text.isEmpty) {
+                    setState(() {
+                      result = 'Please fill in all fields';
+                    });
+                    return;
+                  }
+                  await predict();
                 },
                 style: ButtonStyle(
                   backgroundColor: MaterialStateProperty.all(Colors.pink),
@@ -200,13 +226,25 @@ class _PredictionFormState extends State<PredictionForm> {
                     ),
                   ),
                 ),
-                child: Text(
+                child: const Text(
                   'Predict',
                   style: TextStyle(
                     color: Colors.white,
                   ),
                 ),
               ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          Center(
+            child: Text(
+              result,
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+              textAlign: TextAlign.center,
             ),
           ),
         ],
